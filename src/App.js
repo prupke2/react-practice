0ad9886d619e8a284d7b92a9b2aafddb88173c47
Paul Rupke <paul.rupke@gmail.com>
2020-03-29 18:43:28 -0400

    Change App.js to use a class in order to add state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React testing</h1>
-      <Person name="Jimbo" age="10"/>
-      <Person name="Dolph" age="11"> child text</Person>
-      <Person name="Kearney" age="12"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jimbo', age: 10},
+      { name: 'Dolph', age: 11},
+      { name: 'Kearney', age: 12}
+    ]
+  };
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>React testing</h1>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name="Dolph" age="11"> child text</Person>
+        <Person name="Kearney" age="12"/>
+      </div>
+    );
+  };
 }
 
 export default App;
