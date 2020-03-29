d3ab6ad0c0a26821918239a6923b2bb4ce28030f
Paul Rupke <paul.rupke@gmail.com>
2020-03-29 18:18:12 -0400

    Created Person class, learned how to pass props and children (up to lesson 37)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  return React.createElement('div', null, 'h1', 'Hi, I\'m a React app!')
-    /*
+  return (
     <div className="App">
-
-      <header className="App-header">
-        <h1>hello</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+      <h1>React testing</h1>
+      <Person name="Jimbo" age="10"/>
+      <Person name="Dolph" age="11"> child text</Person>
+      <Person name="Kearney" age="12"/>
     </div>
-    
   );
-  */
 }
 
 export default App;
